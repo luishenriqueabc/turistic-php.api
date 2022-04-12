aec7d5f221aff6559b10a1f6fb6f2e33ce365b66
Luis Henrique <thais.liana2011@gmail.com>
2022-04-12 17:27:17 -0300

    altera

--- models/User.php
+++ models/User.php
@@ -1,15 +1,18 @@
 <?php 
 class User{
+
     public $id;
     public $name;
     public $email;
     public $pass;
+    
     function __construct($id, $name, $email, $pass) {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
         $this->pass = $pass;
     }
+    
     function create(){
         $db = new Database();
         try{
@@ -20,15 +23,7 @@ class User{
             $stmt->bindParam(':pass' , $this->pass);
             $stmt->execute();
             $id = $db->conn->lastInsertId();
-            $result['message'] = "Cadastrado com sucesso";
-            $result['user']['id'] = $id;
-            $result['user']['name'] = $this->name;
-            $result['user']['email'] = $this->email;
-            $result['user']['pass'] = $this->pass;
-            $response = new Output();
-            $response->out($result, 200);
-        
-        
+            return $id;
         }
         catch(PDOException $e){
             $result['message'] = "Erro ao criar" .$e-> getMessage();
@@ -36,70 +31,54 @@ class User{
             $response->out($result, 500);
         }
     }
+
     function delete(){
         $db = new Database();
         try{
             $stmt = $db->conn->prepare("DELETE FROM users WHERE id = :id;");
             $stmt->bindParam(':id' , $this->id);
             $stmt->execute();
-
-        
-            $result['message'] = "Deletado com sucesso";
-            $result['user']['id'] = $this->id;
-            $response = new Output();
-            $response->out($result, 200);
-        
+            return true;
         }
         catch(PDOException $e){
             $result['message'] = "Erro ao deletar" .$e-> getMessage();
             $response = new Output();
             $response->out($result, 500);
         }
-      
     }
-       function update(){
-           $db = new Database();
-           try{
+    
+    function update(){
+        $db = new Database();
+        try{
             $stmt = $db->conn->prepare("UPDATE users SET name = :name,email=:email, pass=:pass WHERE id= :id");
             $stmt->bindParam(':id' , $this->id);
-             $stmt->bindParam(':name' , $this->name);
-             $stmt->bindParam(':email' , $this->email);
-             $stmt->bindParam(':pass' , $this->pass);
+            $stmt->bindParam(':name' , $this->name);
+            $stmt->bindParam(':email' , $this->email);
+            $stmt->bindParam(':pass' , $this->pass);
             $stmt->execute();
-            
-            $id = $db->conn->lastInsertId();
-            $result['message'] = "Update feito com Sucesso";
-            $result['user']['id'] = $this->id;
-            $result['user']['name'] = $this->name;
-            $result['user']['email'] = $this->email;
-            $result['user']['pass'] = $this->pass;
-            $response = new Output();
-            $response->out($result, 200);
-           }
-           catch(PDOException $e){
+            return true;
+        }
+        catch(PDOException $e){
             $result['message'] = "Erro de update." .$e-> getMessage();
             $response = new Output();
             $response->out($result, 500);
         }
-       }
-       function selectAll(){
+    }
+       
+    function selectAll(){
         $db = new Database();
         try{
             $stmt = $db->conn->prepare("SELECT * FROM users");
             $stmt->execute();
             $result = $stmt->fetchAll (PDO::FETCH_ASSOC);
-
-            $response = new Output();
-
-            $response->out($result);
-
+            return $result;
         }
         catch(PDOException $e){
             $result['message'] = "404 - Rota api não encontrada." .$e-> getMessage();
             $response = new Output();
             $response->out($result, 404);
-     }
+        }
     }
 
-  }
+}
 ?>
--- routes.php
+++ routes.php
@@ -7,16 +7,31 @@ $urlClean = substr($url, $lengthStrFolder); // separa a string por partes
 $route = explode('/', $urlClean);
 
 // carrega os autoloades
-require ('helpers/autoloaders.php');
+require('helpers/autoloaders.php');
+// cria objeto de resposta de api
+$response = new Output();
 
-
-if($route[0] == 'user'){
-    require('controllers/UserController.php');
-} elseif($route[0] == 'produto'){
-    require('controllers/ProdutoController.php');
-}else {
+if(!isset($route[0])  ||   !isset($route[1])){
     $result['message'] = "404 - Rota api não encontrada.";
-    $response = new Output();
     $response->out($result, 404);
 }
+
+$controller_name = $route[0];
+$action = str_replace('-', '', $route[1]);
+
+//Checa se o controller existe
+$controller_path = 'controllers/' . $controller_name . 'Controller.php';
+
+// checa se o arquivo do controller existe
+if(file_exists($controller_path)){
+    $controller_class_name = $controller_name.'Controller';
+    $controller = new $controller_class_name();
+    // checa se a action do controller existe
+    if(method_exists($controller, $action)){
+        $controller->$action();
+    }
+}
+
+$result['message'] = "404 - Rota api não encontrada.";
+$response->out($result, 404);
 ?>
